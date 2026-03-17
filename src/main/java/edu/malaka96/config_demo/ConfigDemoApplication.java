53622ad3ed1b3348fd906eac8d05afd3db77bdfb
malaka96 <madhubhashana655@gmail.com>
2026-03-17 15:51:46 +0530

    initial commit

---
+++ src/main/java/edu/malaka96/config_demo/ConfigDemoApplication.java
@@ -0,0 +1,13 @@
+package edu.malaka96.config_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/malaka96/config_demo/ConfigDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.malaka96.config_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
